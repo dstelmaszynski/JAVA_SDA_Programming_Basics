313dadf2b198a1c587b337eecd526c11604933e1
Dominik <stelmaszynski.dominik@gmail.com>
2020-05-19 10:14:41 +0200

    Usuniecie pliku testoweg

--- src/main/java/pl/dstelmaszynski/Main.java
+++ src/main/java/pl/dstelmaszynski/Main.java
@@ -1,12 +1,12 @@
-package pl.dstelmaszynski;
-
+//package pl.dstelmaszynski;
+//
 //Main method - to launch
-public class Main {
-    public static void main(String[] args) {
-        //multiply method
-        Ex1.multiply(5);
-
-        //prime number method
-        Ex2.primeNumb(4);
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//        multiply method
+//        Ex1.multiply(5);
+//
+//        prime number method
+//        Ex2.primeNumb(4);
+//    }
+//}
